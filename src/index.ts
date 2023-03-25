4236c963ca65fa14e95d9118417c1f9759b9fb57
yashweblife <yashweblife20@gmail.com>
2023-03-25 02:20:25 -0500

    add more html files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { Converter } from 'showdown';
 import "./styles/index.scss";
+
+const t = new Converter();
+var op = t.makeHtml(''+document.getElementById('test')?.innerHTML)
+console.log(op) 
+document.body.append(op)
