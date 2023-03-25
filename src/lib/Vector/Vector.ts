fcf34d7684a91b5f7b08127ef8a0d119c1ba3564
nameera906 <82439659+nameera906@users.noreply.github.com>
2023-03-25 23:37:53 +0530

    add vector formulas

--- src/lib/Vector/Vector.ts
+++ src/lib/Vector/Vector.ts
@@ -8,10 +8,12 @@
  * getAngle ()=>number 
  * Dot (Vector)=>void
  * Cross (Vector)=>void
+ * (number, number)=>number
  * RotateX (number)=>void
  * RotateY (number)=>void
  * RotateZ (number)=>void
  */
+    
 export class Vector{
     
     public x:number = 0;
@@ -28,4 +30,54 @@ export class Vector{
         this.y+=v.y;
         this.z+=v.z;
     }
+    public sub = (s:Vector)=>{
+        this.x-=s.x;
+        this.y-=s.y;
+        this.z-=s.z;
+    }
+    public mul = (m:number)=>{
+        this.x*=m;
+        this.y*=m;
+        this.z*=m;
+    }
+    public div = (d:number)=>{
+        this.x/=d;
+        this.y/=d;
+        this.z/=d;
+    }
+    public dist = (v:Vector):number=>{
+        return Math.sqrt((this.x - v.x)**2 + (this.y - v.y)**2 + (this.z - v.z)**2);
+    }
+    public mag = ():number=>{
+        return Math.sqrt((this.x)**2 + (this.y)**2 + (this.z)**2);
+    }
+    public angle = ():number=>{
+        var angle = Math.atan2(this.y, this.x);
+        var degrees = 180 * angle / Math.PI;
+        return (360 + Math.round(degrees)) % 360;
+    }
+    public dot = (v:Vector)=>{
+        this.x*v.x + this.y*v.y + this.z*v.z;
+    }
+    public cross = (v:Vector)=>{
+        this.x*v.y - this.y*v.x;
+        this.y*v.z - this.z*v.y;
+        this.z*v.x - this.x*v.z;
+    }
+    public rotateX = (angle: number) => {
+        this.x;
+        this.y * Math.cos(angle) - this.z * Math.sin(angle);
+        this.y * Math.sin(angle) + this.z * Math.cos(angle);
+      };
+      public rotateY = (angle: number) => {
+        this.x * Math.cos(angle) + this.z * Math.sin(angle);
+        this.y;
+        -this.x * Math.sin(angle) + this.z * Math.cos(angle);
+      };
+      public rotateZ = (angle: number) => {
+        this.x * Math.cos(angle) - this.y * Math.sin(angle);
+        this.x * Math.sin(angle) + this.y * Math.cos(angle);
+        this.z;
+      };
+
 }
