dac856dae69fde0d334e6a1247b16ebe34719379
yashweblife <yashweblife20@gmail.com>
2023-03-28 18:48:47 -0500

    add first demo

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,5 +1,31 @@
-import { SearchBar } from "./search-bar";
+import { Canvas } from "./lib/Canvas/Canvas";
+import { Vector } from "./lib/Vector/Vector";
 import "./styles/canvas.scss";
 import "./styles/common.scss";
 
-const sb = new SearchBar(document.getElementById("search-bar")!);
+const canvasCoordinateSystemDemoDom = document.querySelector(
+  "#canvas-coordinate-system-demo"
+) as HTMLElement;
+const canvasCoordinateSystemDemo = new Canvas({
+  parent: canvasCoordinateSystemDemoDom,
+  size: new Vector(500, 200),
+});
+const mouse = new Vector();
+canvasCoordinateSystemDemo.dom.addEventListener(
+  "mousemove",
+  (e: MouseEvent) => {
+    mouse.x =
+      (e.clientX - canvasCoordinateSystemDemo.bound.left) ;
+    mouse.y =
+      (e.clientY - canvasCoordinateSystemDemo.bound.top);
+  }
+);
+function canvasCoordinateSystemAnimation() {
+  canvasCoordinateSystemDemo.clearAll();
+  canvasCoordinateSystemDemo.start();
+  canvasCoordinateSystemDemo.arc(mouse,1);
+  canvasCoordinateSystemDemo.text(`X:${mouse.x}, Y:${Math.round(mouse.y)}`, mouse, 10, 10)
+  canvasCoordinateSystemDemo.fill();
+  requestAnimationFrame(canvasCoordinateSystemAnimation);
+}
+canvasCoordinateSystemAnimation();
