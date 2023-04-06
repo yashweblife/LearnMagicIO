1373dc4331020378ddc3dce3feba6976d43fe42c
yashweblife <yashweblife20@gmail.com>
2023-04-05 22:44:31 -0500

    add base ball class

--- src/lib/Ball/Ball.ts
+++ src/lib/Ball/Ball.ts
@@ -1,1 +1,32 @@
-export class Ball{}
+import { Canvas } from "../Canvas/Canvas";
+import { Vector } from "../Vector/Vector";
+
+export class Ball {
+  public size: number = 0;
+  public vel: Vector = new Vector();
+  public acc: Vector = new Vector();
+  public mass: number = 1;
+  public color: string = "rgb(0,0,0)";
+  constructor(public pos: Vector = Vector.getRandom(10)) {}
+  public setSize(val: number) {
+    this.size = val;
+  }
+  public setPos(x: number, y: number) {
+    this.pos.x = x;
+    this.pos.y = y;
+  }
+  public setVel(x: number, y: number) {
+    this.vel.x = x;
+    this.vel.y = y;
+  }
+  public setAcc(x: number, y: number) {
+    this.acc.x = x;
+    this.acc.y = y;
+  }
+  public setMass(val: number) {
+    this.mass = val;
+  }
+  public bound(x: number, y: number, w: number, h: number) {}
+  public draw(c: Canvas) {}
+  public update() {}
+}
