7d44b1563793229c8478a023f73de64c17529320
yashweblife <yashweblife20@gmail.com>
2023-04-08 10:58:21 -0500

    remove animate from class

--- src/gravity.ts
+++ src/gravity.ts
@@ -1,68 +1,61 @@
 import { Canvas } from "./lib/Canvas/Canvas";
 import { Vector } from "./lib/Vector/Vector";
 
-
 class Ball {
-  public pos: Vector=new Vector();
-  public vel: Vector=new Vector();
-  public acc: Vector=new Vector();
-  public color: string='red';
-  public size: number=(Math.random()*20)+5; 
+  public pos: Vector = new Vector();
+  public vel: Vector = new Vector();
+  public acc: Vector = new Vector();
+  public color: string = "red";
+  public size: number = Math.random() * 20 + 5;
 
   constructor() {
-    this.pos.x = Math.random()*300
-    this.pos.y = Math.random()*300
-}
+    this.pos.x = Math.random() * 300;
+    this.pos.y = Math.random() * 300;
+  }
 
-public draw(c:Canvas): void {
-  c.start()
-  c.arc(this.pos, this.size)
-  c.fill(this.color)
-  c.end()
-}
+  public draw(c: Canvas): void {
+    c.start();
+    c.arc(this.pos, this.size);
+    c.fill(this.color);
+    c.end();
+  }
 
-public update(): void {
-    this.vel.add(this.acc)
-    this.pos.add(this.vel)
-}
-
+  public update(): void {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
 }
 
 export class GravityDemo {
-  public canvas: Canvas
+  public canvas: Canvas;
 
-  public balls: Ball[]=[]
+  public balls: Ball[] = [];
 
-  constructor(
-    parent: HTMLElement=document.body
-  )
-  {
-    this.canvas=new Canvas({
-      parent: parent, 
-      size: new Vector(300,300)
-    })
-    
+  constructor(parent: HTMLElement = document.body) {
+    this.canvas = new Canvas({
+      parent: parent,
+      size: new Vector(300, 300),
+    });
 
-    for(let i=0; i<10; i++)
-    {
-      const b = new Ball()
-      b.acc.y = 0.05
-      this.balls.push(b)
+    for (let i = 0; i < 10; i++) {
+      const b = new Ball();
+      b.acc.y = 0.05;
+      this.balls.push(b);
     }
   }
-
-  public animate()
-  {
-    // console.log(this.balls)
-    // this.canvas.clearAll()
-    // this.balls.forEach((b: Ball)=>{
-    //   b.update()
-    //   b.draw(this.canvas)
-    // })
-    console.log(1)
-    requestAnimationFrame(this.animate)
+  public update(){
+    this.canvas.clearAll();
+    this.balls.forEach((b:Ball)=>{
+      b.update()
+      b.draw(this.canvas)
+    })
   }
 }
 
-const x = new GravityDemo()
-x.animate()
+const gd = new GravityDemo(document.querySelector("#app") as HTMLElement);
+
+function animate(){
+  gd.update();
+  requestAnimationFrame(animate)
+}
+animate();
