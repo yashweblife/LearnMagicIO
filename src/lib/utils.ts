e91c8d49fe56d90f39a2eff557ec81c803cae014
yashweblife <yashweblife20@gmail.com>
2023-04-15 11:06:36 -0500

    add some more functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,9 @@
 export function lerp(a: number, b: number, t: number) {
   return a + (b - a) * t;
 }
+export function createDOM(a: string = "div") {
+  return document.createElement(a);
+}
+export function toggleDOM(a: string, b: string) {
+  document.querySelector(a)?.classList.toggle(b);
+}
