9d34c4ec28a2377f6d883b65188002e6aa96b7be
yashweblife <yashweblife20@gmail.com>
2023-04-14 22:59:36 -0500

    add a camera class

--- src/lib/Camera/Camera.ts
+++ src/lib/Camera/Camera.ts
@@ -1,1 +1,12 @@
-export class Camera{}
+export class Camera{
+    public video:HTMLVideoElement = document.createElement("video");
+    constructor(){
+        navigator.mediaDevices.getUserMedia({video:{width:300,height:300}}).then((stream:MediaStream)=>{
+            this.video.srcObject = stream;
+            this.video.play();
+        })
+    }
+    public getVideo(){
+        return(this.video)
+    }
+}
