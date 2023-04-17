fb3b96c785d65877b1decb04301d7acd6b2f1881
yashweblife <yashweblife20@gmail.com>
2023-04-16 20:44:11 -0500

    add control page

---
+++ src/pages/genetic_algorithm.ts
@@ -0,0 +1,22 @@
+import { GeneticAlgorithm } from "../Projects/GeneticAlgo";
+
+const app = new GeneticAlgorithm();
+const saveButton = document.querySelector("#save-button")
+const deleteButton = document.querySelector("#delete-button")
+
+saveButton?.addEventListener("click", () => {
+    localStorage.setItem("best-agent",JSON.stringify(app.bestAgent.dna)) 
+});
+deleteButton?.addEventListener("click", () => {
+    localStorage.removeItem("best-agent")
+})
+if(localStorage.getItem("best-agent")){
+    const dna = JSON.parse(localStorage.getItem("best-agent")!);
+    app.createAgentsFromBaseDNA(dna)
+}
+deleteButton?.addEventListener("click", () => {});
+function animation(){
+    app.animate();
+    requestAnimationFrame(animation)
+}
+animation();
